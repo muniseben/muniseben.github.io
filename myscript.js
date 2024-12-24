fe2156702e83c05d7c0eaaaf869017e536d2ebbe
Munise Ben <111012551+muniseben@users.noreply.github.com>
2024-12-24 09:34:52 +0100

    mail-feature

--- myscript.js
+++ myscript.js
@@ -137,3 +137,31 @@ ScrollReveal().reveal(".footer, .group", { delay: 80, origin: "top", interval: 8
 //         });
 //     });
 // })
+
+document.querySelector('.contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            form.reset();
+            document.querySelector('.success-message').style.display = 'block';
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert('Oops! There was a problem submitting your form');
+                }
+            });
+        }
+    }).catch(error => {
+        alert('Oops! There was a problem submitting your form');
+    });
+});
